37444011b69641d8341650354d9aef43fc17e509
ferrantj <jo77_7@yahoo.com>
2014-05-15 21:33:36 -0400

    jk this time i did fo sho

--- Firewall.java
+++ Firewall.java
@@ -146,29 +146,30 @@ class STMFirewall {
 class ParellelFirewall{
 	public static void main(String[] args){
 		if (args.length==2){
+			System.out.println(args[0]+" "+args[1]);
 			if (args[0].equals("1")){
-				args=new String[]{"11","12","5","1","3","3","3822","0.24","0.04","0.96",args[1]};
+				args=new String[]{"11","12","5","1","3","3","3822","0.24","0.04","0.96","2000",args[1]};
 			}
 			else if(args[0].equals("2")){
-				args=new String[]{"12","10","1","3","3","1","2644","0.11","0.09","0.92",args[1]};
+				args=new String[]{"12","10","1","3","3","1","2644","0.11","0.09","0.92","2000",args[1]};
 			}
 			else if(args[0].equals("3")){
-				args=new String[]{"12","10","4","3","6","2","1304","0.10","0.03","0.90",args[1]};
+				args=new String[]{"12","10","4","3","6","2","1304","0.10","0.03","0.90","2000",args[1]};
 			}
 			else if(args[0].equals("4")){
-				args=new String[]{"14","10","5","5","6","2","315","0.08","0.05","0.90",args[1]};
+				args=new String[]{"14","10","5","5","6","2","315","0.08","0.05","0.90","2000",args[1]};
 			}
 			else if(args[0].equals("5")){
-				args=new String[]{"15","14","9","16","7","10","4007","0.02","0.10","0.84",args[1]};
+				args=new String[]{"15","14","9","16","7","10","4007","0.02","0.10","0.84","2000",args[1]};
 			}
 			else if(args[0].equals("6")){
-				args=new String[]{"15","14","9","10","9","9","7125","0.01","0.20","0.77",args[1]};
+				args=new String[]{"15","14","9","10","9","9","7125","0.01","0.20","0.77","2000",args[1]};
 			}
 			else if(args[0].equals("7")){
-				args=new String[]{"15","15","10","13","8","10","5328","0.04","0.18","0.80",args[1]};
+				args=new String[]{"15","15","10","13","8","10","5328","0.04","0.18","0.80","2000",args[1]};
 			}
 			else if(args[0].equals("8")){
-				args=new String[]{"16","14","15","12","9","5","8840","0.04","0.19","0.76",args[1]};
+				args=new String[]{"16","14","15","12","9","5","8840","0.04","0.19","0.76","2000",args[1]};
 			}
 		}
 		final int numAddressesLog=Integer.parseInt(args[0]);
